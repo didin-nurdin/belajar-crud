471744c68cc99c97043d4b86f4ccfdd655387d9b
Didin Nurdin <didinnurdin1989@gmail.com>
2021-04-25 14:35:41 +0700

    menambahkan judul

--- index.php
+++ index.php
@@ -5,8 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar CRUD dengan PHP, MYSQL dan BOOTSTRAP</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+
 </head>
-<body>
-    
+<body>  
+    <!-- Judul -->
+<div class="container text-center" >
+   <h1>CRUD PHP, MYSQL & BOOTSTRAP</h1>
+<h2>INPUT DATA KARYAWAN</h2>
+</div>
+<!-- Akhir Judul -->
+<!-- Membuat card -->
+<div class="container">
+<div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<!-- Akhir membuat card -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
